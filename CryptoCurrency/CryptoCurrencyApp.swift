462f523d644954c9de1dd42fc089540135808627
Mohammed Alsaleh <Malsalehh0@gmail.com>
2022-02-24 06:42:26 -0800

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Mohammed Alsaleh on 23/07/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
